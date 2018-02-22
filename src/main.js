490b80bcc5cc4e7860a7051381037f1eefcf4eac
Angel Rodriguez Romero <aleangrodriguez123@gmail.com>
2018-02-22 08:41:11 -0500

    reset project and added template

--- src/main.js
+++ src/main.js
@@ -4,11 +4,12 @@ import Vue from 'vue'
 import App from './App'
 import router from './router'
 
+Vue.config.productionTip = false
+
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
   router,
-  render: h => h(App),
   components: { App },
   template: '<App/>'
 })
